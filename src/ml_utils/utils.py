cff57b13deb4db7c4945e3c3d9ebda1e02c9aa6d
Florian Ludwig <florian.ludwig@konicaminolta.eu>
2020-05-06 19:04:32 +0200

    Implemented read_parameter_file() function

--- src/ml_utils/utils.py
+++ src/ml_utils/utils.py
@@ -1,5 +1,25 @@
 from contextlib import contextmanager
+from ruamel.yaml import YAML
 import sys, os
+
+
+def read_parameter_file(parameter_file_path: str) -> dict:
+    """
+    Reads the parameters from a yaml file into a dictionary.
+
+    Parameters
+    ----------
+    parameter_file_path: path to a parameter file, which is stored as a yaml file.
+
+    Returns
+    -------
+    params: Dictionary containing the parameters defined in the provided yam file
+    """
+
+    yaml = YAML()
+    with open(parameter_file_path, 'r') as f:
+        params = yaml.load(f)
+    return params
 
 
 @contextmanager
